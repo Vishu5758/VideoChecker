5183bf304e140a6c24e2adeed99bb325c23d34f1
Vishu5758 <vishwambhara@desicrew.in>
2025-04-28 20:18:42 +0530

    Update videochecker.py

--- videochecker.py
+++ videochecker.py
@@ -10,7 +10,7 @@ import logging
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from moviepy.editor import VideoFileClip
+from moviepy import VideoFileClip
 
 # ----------------------------- SETUP -----------------------------
 # Configure logging
